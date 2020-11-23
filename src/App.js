9aac5084117535a823d82be8e960170a72e39c7f
errolgh <ewatsonrei@gmail.com>
2020-11-22 20:35:23 -0500

    Created and styled Nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import AboutUs from './pages/AboutUs'
 import GlobalStyle from './components/GlobalStyle'
+import Nav from './components/Nav'
 // const { default: AboutSection } = require("./components/AboutSection");
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
